d1d70de6cdc1f50b2dd5e8ca414d894ccec11c3e
boarboar <alexander.ershov@gmail.com>
2019-12-23 20:48:07 +0300

    revert char opt

--- term/TFT_ILI9341.cpp
+++ term/TFT_ILI9341.cpp
@@ -230,6 +230,25 @@ void TFT::drawCharLowRAM( INT8U ascii, INT16U poX, INT16U poY)
     for (i=0; i<FONT_SZ; i++, poX+=_size_mask_thick ) {
         temp = simpleFont[ascii-0x20][i];
         y=poY; 
+        
+        for(f=0;f<8;f++, y+=_size_mask_thick)
+        {
+            if((temp>>f)&0x01) // if bit is set in the font mask
+            {
+                sendCMD(0x2A); sendData(poX); sendData(poX+_size_mask_thick-1);
+                sendCMD(0x2B); sendData(y); sendData(y+_size_mask_thick-1);
+                sendCMD(0x2c);
+                TFT_DC_HIGH;
+                TFT_CS_LOW;
+                nb=(_size_mask_thick)*(_size_mask_thick);
+                while(nb--) {
+                  spi_transmit(_fgColorH);
+                  spi_transmit(_fgColorL);
+                }
+            }
+        }
+        
+        /*
                     // todo - try to glue continuois pixels together
         INT8U from=0xff;
         for(f=0;f<8;f++, y+=_size_mask_thick)
@@ -261,7 +280,7 @@ void TFT::drawCharLowRAM( INT8U ascii, INT16U poX, INT16U poY)
                 sendCMD(0x2c);
                 TFT_DC_HIGH;
                 TFT_CS_LOW;
-                nb=(_size_mask_thick*(f-from))*(_size_mask_thick);
+                nb=(_size_mask_thick*(f-from-1))*(_size_mask_thick);
                 while(nb--) {
                   spi_transmit(_fgColorH);
                   spi_transmit(_fgColorL);
@@ -269,19 +288,21 @@ void TFT::drawCharLowRAM( INT8U ascii, INT16U poX, INT16U poY)
                 from=0xff;
               } // stop line
             } // if bit is not set
-           if(from!=0xff) { // unclosed line
+        } //for y
+        
+          if(from!=0xff) { // unclosed line
                 sendCMD(0x2A); sendData(poX); sendData(poX+_size_mask_thick-1);
                 sendCMD(0x2B); sendData(poY+from*_size_mask_thick); sendData(poY+(f-1)*_size_mask_thick-1);
                 sendCMD(0x2c);
                 TFT_DC_HIGH;
                 TFT_CS_LOW;
-                nb=(_size_mask_thick*(f-from))*(_size_mask_thick);
+                nb=(_size_mask_thick*(f-from-1))*(_size_mask_thick);
                 while(nb--) {
                   spi_transmit(_fgColorH);
                   spi_transmit(_fgColorL);
                 }
            }
-        } //for y
+           */
     } // for x
 }
 
